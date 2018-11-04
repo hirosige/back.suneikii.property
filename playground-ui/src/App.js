0090530606b4c1c5404f2bdbd9abc004c691a548
Hiroshige Negishi <hirosige1@gmail.com>
2018-11-04 21:18:55 +0700

    add playground

--- playground-ui/src/App.js
+++ playground-ui/src/App.js
@@ -1,26 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom'
+import Home from './components/Home';
+import Callback from './components/Callback';
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { setContext } from 'apollo-link-context';
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import Logout from './components/Logout';
+import Posts from './components/Posts';
+import {
+  getGraphCoolToken,
+} from './utils/AuthService';
+
+const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cjo0tp8ta5vd40194mfn816ts' })
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: getGraphCoolToken() ? `Bearer ${getGraphCoolToken()}` : "",
+    }
+  }
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <div>
+            <Route exact path='/' component={Home} />
+            <Route path='/callback' component={Callback} />
+            <Route path='/logout' component={Logout} />
+            <Route path='/posts' component={Posts} />
+          </div>
+        </BrowserRouter>
+      </ApolloProvider>
     );
   }
 }
---
+++ playground-ui/src/utils/AuthService.js
@@ -0,0 +1,107 @@
+import decode from 'jwt-decode';
+import auth0 from 'auth0-js';
+const ID_TOKEN_KEY = 'id_token';
+const ACCESS_TOKEN_KEY = 'access_token';
+const GRAPHCOOL_ACCESS_TOKEN = 'graphcool_access_token';
+
+const CLIENT_ID = 'za9gFnUVzet1N0sOL8Y4Q7s5OKS0rQJ9';
+const CLIENT_DOMAIN = 'suneikii.auth0.com';
+const REDIRECT = 'http://localhost:8080/callback';
+const SCOPE = 'openid email';
+const AUDIENCE = 'http://localhost:8080';
+
+var auth = new auth0.WebAuth({
+  clientID: CLIENT_ID,
+  domain: CLIENT_DOMAIN
+});
+
+export function login() {
+  auth.authorize({
+    responseType: 'token id_token',
+    redirectUri: REDIRECT,
+    audience: AUDIENCE,
+    scope: SCOPE
+  });
+}
+
+export function logout() {
+  clearIdToken();
+  clearAccessToken();
+  clearGraphCoolToken();
+}
+
+export function requireAuth(nextState, replace) {
+  if (!isLoggedIn()) {
+    replace({pathname: '/'});
+  }
+}
+
+export function getIdToken() {
+  return localStorage.getItem(ID_TOKEN_KEY);
+}
+
+export function getAccessToken() {
+  return localStorage.getItem(ACCESS_TOKEN_KEY);
+}
+
+export function getGraphCoolToken() {
+  return localStorage.getItem(GRAPHCOOL_ACCESS_TOKEN);
+}
+
+function clearIdToken() {
+  localStorage.removeItem(ID_TOKEN_KEY);
+}
+
+function clearAccessToken() {
+  localStorage.removeItem(ACCESS_TOKEN_KEY);
+}
+
+function clearGraphCoolToken() {
+  localStorage.removeItem(GRAPHCOOL_ACCESS_TOKEN);
+}
+
+// access_token と id_token を展開することを許可するヘルパー関数
+function getParameterByName(name) {
+  let match = RegExp('[#&]' + name + '=([^&]*)').exec(window.location.hash);
+  return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
+
+// access_token をローカルストレージから取得し、保存します
+export function setAccessToken() {
+  let accessToken = getParameterByName('access_token');
+  localStorage.setItem(ACCESS_TOKEN_KEY, accessToken);
+}
+
+export function setGraphCoolToken(accessToken) {
+  localStorage.setItem(GRAPHCOOL_ACCESS_TOKEN, accessToken);
+}
+
+// id_token をローカルストレージから取得し、保存します
+export function setIdToken() {
+  let idToken = getParameterByName('id_token');
+  localStorage.setItem(ID_TOKEN_KEY, idToken);
+}
+
+export function isLoggedIn() {
+  const idToken = getIdToken();
+  return !!idToken && !isTokenExpired(idToken);
+}
+
+function getTokenExpirationDate(encodedToken) {
+  const token = decode(encodedToken);
+  if (!token.exp) { return null; }
+
+  const date = new Date(0);
+  date.setUTCSeconds(token.exp);
+
+  return date;
+}
+
+function isTokenExpired(token) {
+  const expirationDate = getTokenExpirationDate(token);
+  return expirationDate < new Date();
+}
+
+export function isAccountExpired() {
+  return isTokenExpired(clearGraphCoolToken()) || isTokenExpired(getAccessToken())
+}
